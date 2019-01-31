f440d72f327cc0159cc322a596788a93b1e169ca
Sarat Sreepathi <sarat@ornl.gov>
2019-01-31 12:35:52 -0500

    Add example invocation script for Summit

---
+++ examples/site-fullrun.sh
@@ -0,0 +1,3 @@
+# Run this from parent directory
+python ./site_fullrun.py --site US-UMB --caseidprefix test1 --nyears_ad_spinup 200 --nyears_final_spinup 400 --tstep 1 --cpl_bypass --spinup_vars --sitegroup AmeriFlux --machine summit  --ccsm_input /gpfs/alpine/cli115/world-shared/e3sm_olcf/inputdata
+
